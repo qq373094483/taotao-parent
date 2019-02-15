8d72d66367bc8bf7ec78dccedca52af1dd112cfd
qq373094483 <hxyas@qq.com>
2019-02-15 17:13:15 +0800

    添加测试功能

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面展示Controller
+ */
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+}
