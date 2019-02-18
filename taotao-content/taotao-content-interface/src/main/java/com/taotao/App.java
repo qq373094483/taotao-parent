c93568e97d7164d617eb437c07ef2c080e6d3725
qq373094483 <hxyas@qq.com>
2019-02-19 00:11:18 +0800

    完成搭建内容

--- taotao-content/taotao-content-interface/src/main/java/com/taotao/App.java
+++ taotao-content/taotao-content-interface/src/main/java/com/taotao/App.java
@@ -1,13 +1,4 @@
 package com.taotao;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
 }
