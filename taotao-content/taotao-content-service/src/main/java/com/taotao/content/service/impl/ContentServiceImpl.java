69bd0407c1569230a3f40565d21cefd243166768
qq373094483 <hxyas@qq.com>
2019-02-22 15:09:31 +0800

    添加redis缓存

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,12 +1,16 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentCategoryExample;
 import com.taotao.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,6 +20,11 @@ import java.util.List;
 public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper tbContentMapper;
+    @Autowired
+    private JedisClient jedisClient;
+
+    @Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
     @Override
     public TaotaoResult addContent(TbContent tbContent) {
         //补全pojo的属性
@@ -23,14 +32,39 @@ public class ContentServiceImpl implements ContentService {
         tbContent.setUpdated(new Date());
         //插入到内容表
         tbContentMapper.insert(tbContent);
+        //同步缓存
+        //删除对应的缓存信息
+        jedisClient.hdel(INDEX_CONTENT, tbContent.getCategoryId().toString());
         return TaotaoResult.ok();
     }
 
     @Override
     public List<TbContent> getContentByCid(Long cid) {
+        //先查询缓存
+        //添加缓存不能影响正常业务逻辑
+        try{
+            //查询缓存
+            String json = jedisClient.hget(INDEX_CONTENT, cid + "");
+            //查询到结果，把json转换成List返回
+            if (StringUtils.isNotBlank(json)) {
+                return JsonUtils.jsonToList(json, TbContent.class);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //缓存中没有命中，需要查询数据库
         TbContentExample tbContentExample = new TbContentExample();
         TbContentExample.Criteria criteria = tbContentExample.createCriteria();
+        //设置查询条件
         criteria.andCategoryIdEqualTo(cid);
-        return tbContentMapper.selectByExample(tbContentExample);
+        //执行查询
+        List<TbContent> tbContents = tbContentMapper.selectByExample(tbContentExample);
+        //把结果添加到缓存
+        try{
+            jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(tbContents));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return tbContents;
     }
 }
