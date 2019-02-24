8faca42d732d64eb6f6405333ebccb90d806bf98
qq373094483 <hxyas@qq.com>
2019-02-24 18:28:15 +0800

    添加solr功能

---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/mapper/SearchItemMapper.java
@@ -0,0 +1,9 @@
+package com.taotao.search.mapper;
+
+import com.taotao.common.pojo.SearchItem;
+
+import java.util.List;
+
+public interface SearchItemMapper {
+    List<SearchItem> getItemList();
+}
